74cb222aeecf89bb42fde5db6df1b1f54ad439e0
andreamaluta <andreamaluta@gmail.com>
2025-04-07 12:39:40 +0200

    [UPD] bring to js the dom element

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// recupero gli elementi del dom
+const cards = document.querySelectorAll('.card');
+const image = document.querySelectorAll('.img');
+const date = document.querySelectorAll('.date');
+const cardTitle = document.querySelectorAll('.description')
+
