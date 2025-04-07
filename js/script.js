6d5e54509ca98b0a9f3fdecb5ea9dcf982c1318f
andreamaluta <andreamaluta@gmail.com>
2025-04-07 14:36:08 +0200

    [UPD] created the function that create the card

--- js/script.js
+++ js/script.js
@@ -2,8 +2,36 @@
 const cards = document.querySelectorAll('.card');
 const image = document.querySelectorAll('.img');
 const date = document.querySelectorAll('.date');
-const cardTitle = document.querySelectorAll('.description')
+const cardTitle = document.querySelectorAll('.description');
 
-axios.get('https://lanciweb.github.io/demo/api/pictures/').then((resp)=>{
+
+    const createdCard = (objArray) => {
+
+        let card = `<div class="col-4">
+                    <div class="card">
+                       
+                        <div class="pin">
+                            <img src="./img/pin.svg" alt="">
+                        </div>
+                        
+                        <img src=${objArray.url} alt="" class="p-3 img-fluid">
+                        
+                        <div class="content">
+                            
+                            <div class="date">${objArray.date}</div>
+                            
+                             <div class="description">
+                                <h3>${objArray.title}</h3>
+                             </div>
+                            
+                        </div>
+                    </div>
+                </div>`
+
+        return card;            
+    }
+
+   
     
-})
+
+
